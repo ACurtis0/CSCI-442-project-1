fc8b6b42d45c3a1e58c2262d9b031215d0d5991c
elaing1 <112918611+elaing1@users.noreply.github.com>
2022-09-09 10:22:10 -0600

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 {
 	//Check incorrect number of arguments passed to program
 	if(argc < 2 || argc > 3) {
-		fprintf(stderr, "usuage: reverse <input> <output> \n");
+		fprintf(stderr, "usage: reverse <input> <output>\n");
 		exit(1);
 	}
 
@@ -43,14 +43,14 @@ int main(int argc, char *argv[])
 	if (argv[2] != NULL) {
 		//Check if input file is the same as the output file
 		if (strcmp(argv[1], argv[2]) == 0) {
-			fprintf(stderr, "error: input and output file must differ \n");
+			fprintf(stderr, "error: input and output file must differ\n");
 			exit(1);
 		}
 		//Open output file
 		out_file = fopen(argv[2], "w");
 		//Check if output file can be open
 		if (out_file == NULL) {
-			fprintf(stderr, "error: cannot open file %s \n", argv[2]);
+			fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
 			exit(1);
 	}
 	}
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 
 	//If in_file can't open -> stderr
 	if (in_file == NULL) {
-		fprintf(stderr, "error: cannot open file %s \n", argv[1]);
+		fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
 		exit(1);
 	}
 
@@ -131,6 +131,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-
 
